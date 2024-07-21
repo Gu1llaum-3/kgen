7314af5310c43f838cef636cac2268ce5f34e58a
Gu1llaum-3 <archambault.guillaume06@gmail.com>
2024-07-21 22:47:26 +0200

    Modifcations commandes

--- cmd/ingress.go
+++ cmd/ingress.go
@@ -1,52 +1,139 @@
 package cmd
 
 import (
+    "bytes"
     "fmt"
     "os"
+    "text/template"
 
     "github.com/spf13/cobra"
 )
 
-// ingressCmd represents the ingress command
-var ingressCmd = &cobra.Command{
-    Use:   "ingress",
-    Short: "Create a Kubernetes ingress file",
-    Run: func(cmd *cobra.Command, args []string) {
-        content := `
+// Variables to hold flag values
+var ingressName string
+var ingressNamespace string
+var ingressHost string
+var ingressServiceName string
+var ingressServicePort int
+var ingressTemplate bool
+
+// Template for Kubernetes Ingress
+const ingressTemplateContent = `
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
-  name: my-ingress
+  name: {{.Name}}
+  namespace: {{.Namespace}}
 spec:
   rules:
-  - host: my-app.example.com
+  - host: {{.Host}}
     http:
       paths:
       - path: /
         pathType: Prefix
         backend:
           service:
-            name: my-service
+            name: {{.ServiceName}}
             port:
-              number: 80
+              number: {{.ServicePort}}
 `
-        file, err := os.Create("ingress.yaml")
-        if err != nil {
-            fmt.Println("Error creating file:", err)
-            return
-        }
-        defer file.Close()
 
-        _, err = file.WriteString(content)
-        if err != nil {
-            fmt.Println("Error writing to file:", err)
+// ingressCmd represents the ingress command
+var ingressCmd = &cobra.Command{
+    Use:   "ingress",
+    Short: "Create a Kubernetes Ingress file",
+    Run: func(cmd *cobra.Command, args []string) {
+        if ingressTemplate {
+            displayIngressTemplate("ingress", "my-ingress", "default", "example.com", "my-service", 80)
             return
         }
 
-        fmt.Println("Ingress file created successfully.")
+        if ingressName == "" || ingressHost == "" || ingressServiceName == "" || ingressServicePort == 0 {
+            fmt.Println("The --name, --host, --service-name, and --service-port flags are required")
+            return
+        }
+
+        createIngressFile(ingressName, ingressNamespace, ingressHost, ingressServiceName, ingressServicePort)
     },
+}
+
+func createIngressFile(name, namespace, host, serviceName string, servicePort int) {
+    tmpl, err := template.New("ingress").Parse(ingressTemplateContent)
+    if err != nil {
+        fmt.Printf("Error parsing template: %v\n", err)
+        return
+    }
+
+    data := struct {
+        Name        string
+        Namespace   string
+        Host        string
+        ServiceName string
+        ServicePort int
+    }{
+        Name:        name,
+        Namespace:   namespace,
+        Host:        host,
+        ServiceName: serviceName,
+        ServicePort: servicePort,
+    }
+
+    outputFilename := fmt.Sprintf("%s-ingress.yaml", name)
+    file, err := os.Create(outputFilename)
+    if err != nil {
+        fmt.Printf("Error creating file '%s': %v\n", outputFilename, err)
+        return
+    }
+    defer file.Close()
+
+    err = tmpl.Execute(file, data)
+    if err != nil {
+        fmt.Printf("Error executing template: %v\n", err)
+        return
+    }
+
+    fmt.Printf("Ingress file '%s' created successfully.\n", outputFilename)
+}
+
+func displayIngressTemplate(templateType, defaultName, defaultNamespace, defaultHost, defaultServiceName string, defaultServicePort int) {
+    tmpl, err := template.New("display").Parse(ingressTemplateContent)
+    if err != nil {
+        fmt.Printf("Error parsing template: %v\n", err)
+        return
+    }
+
+    data := struct {
+        Name        string
+        Namespace   string
+        Host        string
+        ServiceName string
+        ServicePort int
+    }{
+        Name:        defaultName,
+        Namespace:   defaultNamespace,
+        Host:        defaultHost,
+        ServiceName: defaultServiceName,
+        ServicePort: defaultServicePort,
+    }
+
+    var result bytes.Buffer
+    err = tmpl.Execute(&result, data)
+    if err != nil {
+        fmt.Printf("Error executing template: %v\n", err)
+        return
+    }
+
+    fmt.Printf("Template for %s:\n\n%s\n", templateType, result.String())
 }
 
 func init() {
     rootCmd.AddCommand(ingressCmd)
+
+    // Add flags
+    ingressCmd.Flags().StringVarP(&ingressName, "name", "n", "", "Name of the Ingress")
+    ingressCmd.Flags().StringVarP(&ingressNamespace, "namespace", "N", "default", "Namespace for the Ingress")
+    ingressCmd.Flags().StringVarP(&ingressHost, "host", "H", "", "Host for the Ingress")
+    ingressCmd.Flags().StringVarP(&ingressServiceName, "service-name", "s", "", "Service name for the Ingress backend")
+    ingressCmd.Flags().IntVarP(&ingressServicePort, "service-port", "p", 80, "Service port for the Ingress backend")
+    ingressCmd.Flags().BoolVarP(&ingressTemplate, "template", "t", false, "Display Ingress template")
 }
--- cmd/pvc.go
+++ cmd/pvc.go
@@ -1,46 +1,136 @@
 package cmd
 
 import (
+    "bytes"
     "fmt"
     "os"
+    "text/template"
 
     "github.com/spf13/cobra"
 )
+
+// Variables to hold flag values
+var pvcName string
+var pvcNamespace string
+var pvcTemplate bool
+var pvcStorageSize string
+var pvcAccessMode string
+var pvcStorageClassName string
+
+// Template for Kubernetes PersistentVolumeClaim (PVC)
+const pvcTemplateContent = `
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: {{.Name}}-pvc
+  namespace: {{.Namespace}}
+spec:
+  accessModes:
+    - {{.AccessMode}}
+  resources:
+    requests:
+      storage: {{.StorageSize}}
+  {{- if .StorageClassName }}
+  storageClassName: {{.StorageClassName}}
+  {{- end }}
+`
 
 // pvcCmd represents the pvc command
 var pvcCmd = &cobra.Command{
     Use:   "pvc",
     Short: "Create a Kubernetes PersistentVolumeClaim (PVC) file",
     Run: func(cmd *cobra.Command, args []string) {
-        content := `
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: my-pvc
-spec:
-  accessModes:
-    - ReadWriteOnce
-  resources:
-    requests:
-      storage: 1Gi
-`
-        file, err := os.Create("pvc.yaml")
-        if err != nil {
-            fmt.Println("Error creating file:", err)
-            return
-        }
-        defer file.Close()
-
-        _, err = file.WriteString(content)
-        if err != nil {
-            fmt.Println("Error writing to file:", err)
+        if pvcTemplate {
+            displayPVCTemplate("pvc", "my-pvc", "default", "1Gi", "ReadWriteOnce", "")
             return
         }
 
-        fmt.Println("PersistentVolumeClaim file created successfully.")
+        if pvcName == "" {
+            fmt.Println("The --name flag is required")
+            return
+        }
+
+        createPVCFile(pvcName, pvcNamespace, pvcStorageSize, pvcAccessMode, pvcStorageClassName)
     },
+}
+
+func createPVCFile(name, namespace, storageSize, accessMode, storageClassName string) {
+    tmpl, err := template.New("pvc").Parse(pvcTemplateContent)
+    if err != nil {
+        fmt.Printf("Error parsing template: %v\n", err)
+        return
+    }
+
+    data := struct {
+        Name            string
+        Namespace       string
+        StorageSize     string
+        AccessMode      string
+        StorageClassName string
+    }{
+        Name:            name,
+        Namespace:       namespace,
+        StorageSize:     storageSize,
+        AccessMode:      accessMode,
+        StorageClassName: storageClassName,
+    }
+
+    outputFilename := fmt.Sprintf("%s-pvc.yaml", name)
+    file, err := os.Create(outputFilename)
+    if err != nil {
+        fmt.Printf("Error creating file '%s': %v\n", outputFilename, err)
+        return
+    }
+    defer file.Close()
+
+    err = tmpl.Execute(file, data)
+    if err != nil {
+        fmt.Printf("Error executing template: %v\n", err)
+        return
+    }
+
+    fmt.Printf("PersistentVolumeClaim file '%s' created successfully.\n", outputFilename)
+}
+
+func displayPVCTemplate(templateType, defaultName, defaultNamespace, defaultStorageSize, defaultAccessMode, defaultStorageClassName string) {
+    tmpl, err := template.New("display").Parse(pvcTemplateContent)
+    if err != nil {
+        fmt.Printf("Error parsing template: %v\n", err)
+        return
+    }
+
+    data := struct {
+        Name            string
+        Namespace       string
+        StorageSize     string
+        AccessMode      string
+        StorageClassName string
+    }{
+        Name:            defaultName,
+        Namespace:       defaultNamespace,
+        StorageSize:     defaultStorageSize,
+        AccessMode:      defaultAccessMode,
+        StorageClassName: defaultStorageClassName,
+    }
+
+    var result bytes.Buffer
+    err = tmpl.Execute(&result, data)
+    if err != nil {
+        fmt.Printf("Error executing template: %v\n", err)
+        return
+    }
+
+    fmt.Printf("Template for %s:\n\n%s\n", templateType, result.String())
 }
 
 func init() {
     rootCmd.AddCommand(pvcCmd)
+
+    // Add flags
+    pvcCmd.Flags().StringVarP(&pvcName, "name", "n", "", "Name of the PersistentVolumeClaim")
+    pvcCmd.Flags().StringVarP(&pvcNamespace, "namespace", "N", "default", "Namespace for the PersistentVolumeClaim")
+    pvcCmd.Flags().BoolVarP(&pvcTemplate, "template", "t", false, "Display PersistentVolumeClaim template")
+    pvcCmd.Flags().StringVarP(&pvcStorageSize, "storagesize", "s", "1Gi", "Storage size for the PersistentVolumeClaim")
+    pvcCmd.Flags().StringVarP(&pvcAccessMode, "accessmode", "a", "ReadWriteOnce", "Access mode for the PersistentVolumeClaim")
+    pvcCmd.Flags().StringVarP(&pvcStorageClassName, "storageclassname", "c", "", "Storage class name for the PersistentVolumeClaim")
 }
